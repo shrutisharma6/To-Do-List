6b3005f5c5097779e61bde028f5adc1054754d1e
shrutisharma6 <reachshruti611@gmail.com>
2023-10-31 16:55:21 +0530

    Added CRUD ops on Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,45 @@
 class TasksController < ApplicationController
+    
+    before_action :set_task, only: [:edit, :update, :destroy]
+
     def new
         @task = Task.new
     end
 
     def create
         @task = Task.new(task_params)
+        @task.save
+        redirect_to tasks_path
     end
 
     def index
-        @tasks = Tasks.all
+        @tasks = Task.all
     end
     
     def edit
     end
 
     def update
+        if @task.update(task_params)
+            flash[:notice]= "Task was updated successfully."
+            redirect_to task_path
+        else
+            render 'edit'
+        end
     end
 
     def destroy
+        @task.destroy
+        redirect_to tasks_path
     end
 
     private
 
         def task_params
             params.require(:task).permit(:name)
+        end
+
+        def set_task
+            @task=Task.find(params[:id])
         end
 end
